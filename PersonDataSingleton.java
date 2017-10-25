cc28865c6cff3e54986c0f59eb94ed18f5acc2ea
bgsahan <bgsahan@yahoo.com>
2017-10-25 20:29:17 +0300

    Update PersonDataSingleton.java

--- PersonDataSingleton.java
+++ PersonDataSingleton.java
@@ -1,42 +1,40 @@
-PersonDataSingleton.java 
-1    package com.example.lenovo.socialbars; 
-2     
-3    import java.util.ArrayList; 
-4     
-5    /** 
-6     * Created by Lenovo on 4.6.2017. 
-7     */ 
-8     
-9    public class PersonDataSingleton 
-10   { 
-11       private static final PersonDataSingleton ourInstance = new PersonDataSingleton(); 
-12    
-13   //Declaring PersonData ArrayList 
-14       private ArrayList<PersonData> mPersonDataList; 
-15   //*** 
-16    
-17       public static PersonDataSingleton getInstance() { 
-18           return ourInstance; 
-19       } 
-20    
-21   //Singleon private constructor method 
-22       private PersonDataSingleton() 
-23       { 
-24           mPersonDataList = new ArrayList<PersonData>(); 
-25       } 
-26   //*** 
-27    
-28   //Getters & Setters 
-29       public ArrayList<PersonData> getPersonDataList() 
-30       { 
-31           return mPersonDataList; 
-32       } 
-33    
-34       public void setPersonDataList(ArrayList<PersonData> mPersonDataList) 
-35       { 
-36           this.mPersonDataList = mPersonDataList; 
-37       } 
-38   //*** 
-39    
-40   } 
-41   
+package com.example.lenovo.socialbars; 
+ 
+import java.util.ArrayList; 
+ 
+/** 
+ * Created by Lenovo on 4.6.2017. 
+ */ 
+ 
+public class PersonDataSingleton 
+{ 
+    private static final PersonDataSingleton ourInstance = new PersonDataSingleton(); 
+ 
+    //Declaring PersonData ArrayList 
+    private ArrayList<PersonData> mPersonDataList; 
+//*** 
+ 
+    public static PersonDataSingleton getInstance() { 
+        return ourInstance; 
+    } 
+ 
+    //Singleon private constructor method 
+    private PersonDataSingleton() 
+    { 
+        mPersonDataList = new ArrayList<PersonData>(); 
+    } 
+//*** 
+ 
+    //Getters & Setters 
+    public ArrayList<PersonData> getPersonDataList() 
+    { 
+        return mPersonDataList; 
+    } 
+ 
+    public void setPersonDataList(ArrayList<PersonData> mPersonDataList) 
+    { 
+        this.mPersonDataList = mPersonDataList; 
+    } 
+//*** 
+ 
+}
